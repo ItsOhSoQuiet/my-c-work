0faacf43ed44c18459908d4f8f6ac4393dd90cdd
Matthew Kluk <spongebob1894@hotmail.com>
2017-08-04 13:37:49 -0600

    Started some early tests, including header files and makefiles

---
+++ early_tests/header_test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "header_test.h"
+
+void funct_text(){
+  printf("Text from a function.\n");
+}
---
+++ early_tests/header_to_file.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "header_test.h"
+
+int main(int argc, char *argv[]){
+  funct_text();
+  return 0;
+}
