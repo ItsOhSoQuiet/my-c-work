b5cec7726a8138f7c988ea7c04dfdfd2eda04fb4
Matthew Kluk <spongebob1894@hotmail.com>
2017-08-11 17:08:42 -0600

    Improved fgets.c, though it still can't accept -1 as input

--- early_tests/fgets_test.c
+++ early_tests/fgets_test.c
@@ -5,7 +5,6 @@ long int num_entry(void){
   char buff[100];
   char *endpoint;
   long int i;
-  printf("Enter a number: ");
   if (fgets(buff, 100, stdin) == NULL) {
     printf("Done!\n\n");
     return -1;
@@ -21,32 +20,43 @@ long int num_entry(void){
 }
 
 int main(int argc, char *argv[]){
-  long int numArray[100];
-  int i, j;
-  long int k;
-  i = 0;
-  j = 0;
-  while (j != -1){
-    k = num_entry();
-    if (k == -1){
-      break;
-    } else {
-      numArray[i] = k;
-      i++;
-      if (i == 100){
-        break;
-      }
-    }
-  }
-  if (i == 0){
-    printf("No numbers entered.\n");
+  int arraySize;
+  char buff[10];
+  printf("Enter the amount of integers that you wish to enter: ");
+  fgets(buff, 10, stdin);
+  arraySize = atoi(buff);
+  if (arraySize < 1){
+    printf("Enter an actual integer for how many numbers you wish to enter.\n");
     return 0;
   } else {
-    int l;
-    for (l = 0; l < i; l++){
-      printf("%li ", numArray[l]);
+    long int numArray[arraySize];
+    int i, j;
+    long int k;
+    i = 0;
+    j = 0;
+    while (j != -1){
+      printf("Enter number %d, or enter blank line to exit: ", i+1);
+      k = num_entry();
+      if (k == -1){
+        break;
+      } else {
+        numArray[i] = k;
+        i++;
+        if (i == arraySize){
+          break;
+        }
+      }
     }
-    printf("\n");
-    return 0;
+    if (i == 0){
+      printf("No numbers entered.\n");
+      return 0;
+    } else {
+      int l;
+      for (l = 0; l < i; l++){
+        printf("%li ", numArray[l]);
+      }
+      printf("\n");
+      return 0;
+    }
   }
 }
